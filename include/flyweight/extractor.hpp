a406997a4cba5e97e18dc317d87ee4fcc46725bd
Tres Walsh <tres.walsh@mnmlstc.com>
2013-11-12 11:43:26 -0800

    Add flyweight::extractor<T> header file

---
+++ include/flyweight/extractor.hpp
@@ -0,0 +1,22 @@
+#ifndef FLYWEIGHT_EXTRACTOR_HPP
+#define FLYWEIGHT_EXTRACTOR_HPP
+
+namespace flyweight {
+inline namespace v1 {
+
+/* Extracts a 'key-type' for a given T */
+template <class T>
+struct extractor {
+  using argument_type = core::add_const_t<core::add_lvalue_reference_t<T>>;
+  using result_type = argument_type;
+
+  constexpr extractor () noexcept { }
+
+  result_type operator () (argument_type argument) const noexcept {
+    return argument;
+  }
+};
+
+}} /* namespace flyweight::v1 */
+
+#endif /* FLYWEIGHT_EXTRACTOR_HPP */
