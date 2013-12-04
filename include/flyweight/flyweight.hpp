36a01af98b453a1dcd95d1f7c2840b9de6ab411b
Tres Walsh <tres.walsh@mnmlstc.com>
2013-12-04 00:19:22 -0800

    Add amalgamation header

---
+++ include/flyweight/flyweight.hpp
@@ -0,0 +1,8 @@
+#ifndef FLYWEIGHT_AMALGAMATION_HPP
+#define FLYWEIGHT_AMALGAMATION_HPP
+
+#include <flyweight/extractor.hpp>
+#include <flyweight/object.hpp>
+#include <flyweight/cache.hpp>
+
+#endif /* FLYWEIGHT_AMALGAMATION_HPP */
